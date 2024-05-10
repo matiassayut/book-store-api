8f99aa8504aa0a4783c7e15822e570a92c822fc9
Matias Sayut <matiassayut@gmail.com>
2024-05-09 21:44:26 -0300

    Modify BookRepository

--- src/main/java/com/bookstore/bookstoreapi/repositories/IBookRepository.java
+++ src/main/java/com/bookstore/bookstoreapi/repositories/IBookRepository.java
@@ -2,8 +2,18 @@ package com.bookstore.bookstoreapi.repositories;
 
 import com.bookstore.bookstoreapi.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository("bookRepository")
 public interface IBookRepository extends JpaRepository<Book, Integer> {
+
+    public abstract List<Book> findByAuthor(String name);
+
+    @Query("SELECT b FROM Book b JOIN b.genres g WHERE g.id = :genreId")
+    List<Book> findByGenreId(@Param("genreId") int genreId);
+
 }
